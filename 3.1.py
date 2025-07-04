be59b14974b78295576c0ebaf647f98a49a5df64
Lee Jia Qi <ljqleejiaqi@gmail.com>
2025-07-04 16:41:04 +0800

    WILSON first prototype of 3.1

--- 3.1.py
+++ 3.1.py
@@ -15,7 +15,7 @@ def vid():
     video = hula_video(hula_api=uapi,display=False)
     detector = tflite_detector(model="nytc-balanced-4Aug.tflite",label="label.txt")
     video.video_mode_on()
-    for i in range(500):
+    while True:
         frame = video.get_video()
         object_found, frame = detector.detect(frame)
         if not object_found is None:
@@ -40,3 +40,37 @@ else:
     print('success')
 
     threading.Thread(target=vid).start()
+#5
+uapi.single_fly_takeoff()
+uapi.single_fly_forward(60)
+uapi.single_fly_left(55)
+uapi.single_fly_touchdown()
+#6
+uapi.single_fly_takeoff()
+uapi.single_fly_forward(60)
+uapi.single_fly_touchdown()
+#1
+uapi.single_fly_takeoff()
+uapi.single_fly_right(55)
+uapi.single_fly_touchdown()
+#2\
+uapi.single_fly_takeoff()
+uapi.single_fly_forward(60)
+uapi.single_fly_touchdown()
+#7
+uapi.single_fly_takeoff()
+uapi.single_fly_left(55)
+uapi.single_fly_touchdown()
+#3
+uapi.single_fly_takeoff()
+uapi.single_fly_right(120)
+uapi.single_fly_back(60)
+uapi.single_fly_touchdown()
+#4
+uapi.single_fly_takeoff()
+uapi.single_fly_back(60)
+uapi.single_fly_touchdown()
+#8
+uapi.single_fly_takeoff()
+uapi.single_fly_left(65)
+uapi.single_fly_touchdown()
